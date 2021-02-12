b20dfbd56939364513e38b0381a1c5fb24efa09e
Nathan Douglas <github@tenesm.us>
2021-02-11 19:03:15 -0500

    Adds deploy.sh script.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe;
+sam package \
+  --template-file template.yml \
+  --output-template-file output_template.yml \
+  --s3-bucket s3mail.artifacts \
+  --debug;
+sam deploy \
+  --profile s3mail \
+  --region us-east-1 \
+  --template-file ./output_template.yml \
+  --stack-name "s3mail" \
+  --s3-bucket s3mail.artifacts \
+  --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND;
