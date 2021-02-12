1d89989fdfffc09808c0c199df0b1d123559762d
Nathan Douglas <github@tenesm.us>
2021-02-11 19:55:21 -0500

    Updates template and deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -xe;
 sam package \
+  --profile s3mail \
+  --region us-east-1 \
   --template-file template.yml \
   --output-template-file output_template.yml \
   --s3-bucket s3mail.artifacts \
